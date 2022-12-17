bea93e53363d98c7210fb2c26146b4fdb106569e
Themis Theodoratos <themistheodoratos@gmail.com>
2022-12-17 22:16:56 +0000

    Ingredients model and GET API method

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,7 +7,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import (
-    Recipe, Tag)
+    Recipe, Tag, Ingredient)
 from recipe import serializers
 
 
@@ -46,4 +46,16 @@ class TagViewSet(
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage Ingredients in the database."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticate user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
