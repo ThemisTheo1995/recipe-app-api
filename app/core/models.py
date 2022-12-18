5d1f9bb7a1e602973a4fd07071a876aa161fd796
Themis Theodoratos <themistheodoratos@gmail.com>
2022-12-18 01:36:52 +0000

    Update recipe API to handle ingredients

--- app/core/models.py
+++ app/core/models.py
@@ -48,7 +48,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 class Recipe(models.Model):
     """Recipe object."""
     class Meta:
-        verbose_name_plural = 'Recipies'
+        verbose_name_plural = 'Recipes'
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
