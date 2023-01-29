cd334ac58d2ea900aeab4703410b602e604a6f50
Themis Theodoratos <themistheodoratos@gmail.com>
2023-01-29 20:31:59 +0000

    Prepare Django settings for prod

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-28yj%t_cdo4$ixdyf2kw$*51%+og=^(%i#)!2a92v&+1yo96!y'  # noqa: E501
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')  # noqa: E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
